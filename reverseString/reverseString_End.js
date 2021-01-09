a690cafdd46e4f8bb5f035e30cf8217f6b30463c
Liv Codes <oliviareeddev@gmail.com>
2021-01-08 21:07:11 -0500

    added steps

--- reverseString/reverseString_End.js
+++ reverseString/reverseString_End.js
@@ -26,21 +26,22 @@ console.log(reverseString("hello")); // returns "olleh"
 function reverseString2(str) {
   let revString = '';
   for(let i = str.length - 1; i >= 0; i -= 1) {
-    revString = revString + str[i];
+    revString += str[i];
   }
   return revString;
 }
 console.log(reverseString2("friend")); // returns "dneirf"
     
             /* Steps to solve:
-            Step 1: split the string by each character using the .split() method.
-                    "The split() method divides a String into an ordered list of substrings, 
-                    puts these substrings into an array, and returns the array." -MDN website
-            Step 2: Now that the string is split into an array, use the .reverse() method 
-                    to reverse the elements in the array.
-            Step 3: Your new array has every element in an reversed order. Use the .join() method
-                    to rejoin the characters into a string
-            Step 4: Invoke the function
+            Step 1: Declare a variable called revString and initialize it to an empty string
+            Step 2: For the first expression in the for loop declare a counter variable 
+                    and set it equal to the length of the string minus 1.   
+            Step 3: The for loop should run as long as the counter variable is greater than or equal to zero, 
+                    (zero is the index of the 1st letter in the string) so the second expression should be the counter variable >= 0.
+            Step 4: The last expression decrements the counter variable in this case by 1.
+            Step 5: Inside the body of the For Loop add each element to revString on each iteration
+                    by using the += operator.
+            Step 6: Invoke the function
             */
     
 // Solution 3: Incrementing For Loop 
@@ -82,3 +83,43 @@ console.log(reverseString4("Game on")); // returns "no emaG"
                     to rejoin the characters into a string
             Step 4: Invoke the function
             */
+
+
+// Solution 5: High Order Array Method -> ForEach() 
+function reverseString5(str) {
+        let revString = '';
+        str.split('').forEach(function(char) {         // Arrow function refactor: str.split('').forEach(char => revString = char + revString);
+          revString =  char + revString ;
+        });
+        return revString;
+      }
+      console.log(reverseString5("For Each Letter")); // returns "retteL hcaE roF"
+                  /* Steps to solve:
+                  Step 1: split the string by each character using the .split() method.
+                          "The split() method divides a String into an ordered list of substrings, 
+                          puts these substrings into an array, and returns the array." -MDN website
+                  Step 2: Now that the string is split into an array, use the .reverse() method 
+                          to reverse the elements in the array.
+                  Step 3: Your new array has every element in an reversed order. Use the .join() method
+                          to rejoin the characters into a string
+                  Step 4: Invoke the function
+                  */
+
+
+// Solution 6: High Order Array Method -> reduce() 
+function reverseString6(str) {
+        return str.split('').reduce(function(revString, char) {         // Arrow function refactor: return str.split('').reduce((revString, char) => char + revString, '');
+          return char + revString ;
+        }, '');
+      }
+      console.log(reverseString6("Another One")); // returns "enO rehtonA"
+                  /* Steps to solve:
+                  Step 1: split the string by each character using the .split() method.
+                          "The split() method divides a String into an ordered list of substrings, 
+                          puts these substrings into an array, and returns the array." -MDN website
+                  Step 2: Now that the string is split into an array, use the .reverse() method 
+                          to reverse the elements in the array.
+                  Step 3: Your new array has every element in an reversed order. Use the .join() method
+                          to rejoin the characters into a string
+                  Step 4: Invoke the function
+                  */
---
+++ reverseString/reverseString_Start.js
@@ -0,0 +1,13 @@
+/* Challenge #1 : reverseString
+ Write a function called reverseString where given a string as a parameter 
+ will reverse the characters of the string and return the reversed string
+
+ Example:
+ input: "hello"  -> output: "olleh"
+*/
+
+
+function reverseString(str) {
+    
+  }
+  console.log(reverseString("hello")); // returns "olleh"
